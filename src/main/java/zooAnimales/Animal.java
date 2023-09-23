202d3e7483633922ac77e7fafbcd16a456ca3f2e
segomezz <141938389+segomezz@users.noreply.github.com>
2023-09-23 17:11:44 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -8,17 +8,72 @@ public class Animal {
 	private String habitat;
 	private String genero;
 	private Zona zona;
-Animal(){
-	totalAnimales+=1
+public Animal(){
+	totalAnimales+=1;
 }
-Animal(String nombre,int edad,String habitat,String genero,Zona zona){
+public Animal(String nombre,int edad,String habitat,String genero,Zona zona){
 	this.nombre=nombre;
 	this.edad=edad;
 	this.habitat=habitat;
 	this.genero=genero;
 	this.zona=null;
-	totalAnimales+=1
+	totalAnimales+=1;
 }
+public movimiento() {
+	return "desplazarse";
+}
+public totalAnimales () {
+	return totalAnimales;
+}
+public totalPorTipo() {
+	String resultado = String.format("Mamífero: %d%nAve: %d%nReptil: %d%nPez: %d%nAnfibio; %d%n", Mamifero.cantidadMamiferos(), Ave.cantidadAve(),Reptil.cantidadReptiles(),Pez.cantidadPeces(),Anfibio.cantidadAnfibios());
+return resultado;
+}
+public String toString() {
+	String message = String.format("Mi nombre es %s, tengo una edad de %s, habito en %s y mi genero es %s", nombre, edad, habitat, genero);
+	
+	if (zona != null) {
+		message = String.format("%s, la zona en la que me ubico es %s, en el %s", message, zona.getNombre(), zona.getZoo().getNombre());
+	}
+	
+	return message;
 
 
+	
+	public static int getTotalAnimales() {
+		return totalAnimales;
+	}
+	public static void setTotalAnimales(int totalAnimales) {
+		Animal.totalAnimales = totalAnimales;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getHabitat() {
+		return habitat;
+	}
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public Zona getZona() {
+		return zona;
+	}
+	public void setZona(Zona zona) {
+		this.zona = zona;
+	}
 }
