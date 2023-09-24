a7e3bfe93a5e77d077aa096364bf948f0af9b795
segomezz <141938389+segomezz@users.noreply.github.com>
2023-09-23 20:03:11 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -25,7 +25,7 @@ public String movimiento() {
 public int totalAnimales () {
 	return totalAnimales;
 }
-public static String totalPorTipo() {
+public String totalPorTipo() {
 	String resultado = String.format("Mamifero: %d%nAve: %d%nReptil: %d%nPez: %d%nAnfibio: %d%n", Mamifero.cantidadMamiferos(), Ave.cantidadAves(),Reptil.cantidadReptiles(),Pez.cantidadPeces(),Anfibio.cantidadAnfibios());
 return resultado;
 }
